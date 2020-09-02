c38e2efda43fd03930a2d046eae7d961d85e6fac
Angel <pabloski085@gmail.com>
2020-09-02 23:58:54 +0200

    02/09/2020 23:58

---
+++ TraballosClase/src/Circunferencia.java
@@ -0,0 +1,20 @@
+
+public class Circunferencia {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		double radio = 3;
+		double pi = Math.PI;
+		double resultadoArea = pi*Math.pow(radio, 2);
+		double resultadoPerimetro = 2*pi*radio;
+
+		
+		
+		System.out.println("El area de la circunferencia es: " + resultadoArea + "\n" + "El perimetro de la circunferencia es: " + resultadoPerimetro);
+		
+
+		
+	}
+
+}
