08586c693b2e331dc4c8e94c055e075bf31771b9
sabad <sabad@host.docker.internal>
2020-09-03 23:09:39 +0200

    03/09/2020 23:09

---
+++ Coeficiente/src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//Dados los coeficientes de una ecucaion de segundo grado:
+		//- Mostrar si tiene solucion unica
+		//- Mostrar si tiene solucion doble
+		//- Mostrar si no tiene solucion posible
+		
+		
+		
+	}
+
+}
