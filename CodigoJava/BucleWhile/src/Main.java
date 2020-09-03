cbe0006b3464471261e3a61e84b2838dca76fe4e
Mr-Ragger <58849894+Mr-Ragger@users.noreply.github.com>
2020-09-03 13:48:08 +0200

    Add files via upload

---
+++ CodigoJava/BucleWhile/src/Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int veces = 0;
+		
+		while(veces < 5 ) {
+			
+			System.out.println("Este mensaje se repite: " + (veces + 1) + "veces");
+			
+			veces++;
+		}
+		
+	}
+
+}
