b92fb30a8e51e10f3157ffac40062e26998831c8
Curso Intro <Curso Intro@192.168.1.105>
2020-09-07 13:11:55 +0200

    Calculo de area de un circulo usando objetos y metodos. V1.1

--- CalcularAreaCirculoPOO/src/Circulo.java
+++ CalcularAreaCirculoPOO/src/Circulo.java
@@ -3,15 +3,19 @@ public class Circulo {
 
 	double radio;
 	double area;
-	
-	public double calcularArea(int radio) {
-		
+
+	public Circulo(int radio) {
+
 		this.radio = radio;
-		
-		this.area = Math.PI * Math.pow(this.radio, 2);
-		
-		return this.area;
-		
+
 	}
-	
+
+	public double calcularArea() {
+
+		this.area = Math.PI * Math.pow(this.radio, 2);
+
+		return this.area;
+
+	}
+
 }
--- CalcularAreaCirculoPOO/src/Main.java
+++ CalcularAreaCirculoPOO/src/Main.java
@@ -4,9 +4,9 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Circulo miCirculo = new Circulo();
+		Circulo miCirculo = new Circulo(12);
 		
-		miCirculo.calcularArea(3);
+		miCirculo.calcularArea();
 		
 		System.out.println("El area del circulo: " + miCirculo.area);
 	}
